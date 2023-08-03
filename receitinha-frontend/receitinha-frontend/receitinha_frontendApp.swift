ac4a411ec906c873ec52cad4656cf5e3d3027971
Felipo <54708833+gabrielfelipo@users.noreply.github.com>
2023-08-03 10:37:30 -0300

    Base project

---
+++ receitinha-frontend/receitinha-frontend/receitinha_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  receitinha_frontendApp.swift
+//  receitinha-frontend
+//
+//  Created by gabrielfelipo on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct receitinha_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
