bb3779c17cc002b0128d7a622809db8944ac8f9d
apfdamascena <apfd@cin.ufpe.br>
2023-08-09 09:31:05 -0300

    feature(achievements): create models to achievement

--- receitinha-frontend/Modules/Achievement/AchievementViewModel.swift
+++ receitinha-frontend/Modules/Achievement/AchievementViewModel.swift
@@ -12,5 +12,14 @@ import SwiftUI
 
 final class AchievementViewModel: ObservableObject {
     
-//    @Published var 
+//    @Published var
+    
+    @Published var achievements: [Achievement] = [
+        Achievement(image: AssetImage.banana, name: "Mestre Banana", avaiable: .completed),
+        Achievement(image: AssetImage.banana_split, name: "Mestre Split", avaiable: .blocked),
+        Achievement(image: AssetImage.burguer, name: "Mestre Burguer", avaiable: .blocked),
+        Achievement(image: AssetImage.ice, name: "Mestre Ice", avaiable: .blocked),
+        Achievement(image: AssetImage.cookies, name: "Mestre Cookie", avaiable: .blocked),
+        Achievement(image: AssetImage.muffin, name: "Mestre Muffin", avaiable: .blocked)
+    ]
 }
