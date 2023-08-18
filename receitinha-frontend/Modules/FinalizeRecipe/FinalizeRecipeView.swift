dd2ddcf08d297cf462e7626a07088dc3e8e0dca6
apfdamascena <apfd@cin.ufpe.br>
2023-08-18 10:11:30 -0300

    feature(finalize-recipe): create view

--- receitinha-frontend/Modules/FinalizeRecipe/FinalizeRecipeView.swift
+++ receitinha-frontend/Modules/FinalizeRecipe/FinalizeRecipeView.swift
@@ -13,9 +13,42 @@ struct FinalizeRecipeView: View {
     @ObservedObject private var viewModel = FinalizeRecipeViewModel()
        
     var body: some View {
-        VStack {
-            Text("funcionando")
+        ZStack {
+            
+            VStack {
+                Text("Parabéns")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.top, 40)
+                
+                Spacer()
+                    .frame(height: 60)
+                
+                Image(AssetImage.cenourinha)
+                
+                VStack(alignment: .center, spacing: 4) {
+                    Text("Você concluiu esta receita!")
+                        .font(.system(size: 22))
+                        .fontWeight(.bold)
+                    
+                    Text("Lembre-se de organizar a cozinha e aproveite sua comida!")
+                        .font(.system(size: 22))
+                        .multilineTextAlignment(.center)
+                }
+                .padding(.top, 60)
+                .padding(.horizontal, 12)
+                
+                Spacer()
+                
+                ReceitinhaButton(spacing: 16,
+                                 style: .primary,
+                                 title: "Finalizar Receita"){
+                    print("oi")
+                }
+            }
+
         }
+        .background(Color(AssetColor.blue_100))
     }
 
 }
