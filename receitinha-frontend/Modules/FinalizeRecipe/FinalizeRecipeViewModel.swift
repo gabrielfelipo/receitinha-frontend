2e77813eed8196044fc4911c35c8d4926b633ffd
Felipo <gabriel.felipo2010@gmail.com>
2023-08-30 02:05:26 -0300

    feature: nova integração de receitas ao backend

--- receitinha-frontend/Modules/FinalizeRecipe/FinalizeRecipeViewModel.swift
+++ receitinha-frontend/Modules/FinalizeRecipe/FinalizeRecipeViewModel.swift
@@ -7,9 +7,26 @@
 //
 
 import SwiftUI
+import Combine
 
 // MARK: Variables to watch mark as Published. ViewModel also calls API/Core Data
 
 final class FinalizeRecipeViewModel: ObservableObject {
     @Published var example: String?
+    
+    private var caller = APICaller()
+    
+    @Published var isSomethingWrong = false
+    private var url : URL = APIBuilder().routeTo(.receitas).build()
+    
+    private var bag = Set<AnyCancellable>()
+    
+    func getRecipes() async -> Result<GetReceitas, APICallerError> {
+        var api = APIBuilder()
+            .routeTo(.receitas)
+            .build()
+        let request = caller.createRequest(with: api, and: .get)
+        let response = await caller.peform(request, expecting: GetReceitas.self)
+        return response
+    }
 }
