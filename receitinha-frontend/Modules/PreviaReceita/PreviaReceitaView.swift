7c81be9dfc788c983ff4dc8d72e5bc5bf2644c6c
Felipo <gabriel.felipo2010@gmail.com>
2023-08-29 21:21:41 -0300

    feature: recipe-preview

--- receitinha-frontend/Modules/PreviaReceita/PreviaReceitaView.swift
+++ receitinha-frontend/Modules/PreviaReceita/PreviaReceitaView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct PreviaReceitaView: View {
+    var ingredientsList = ["colher", "pao", "faca"]
+    
     var body: some View {
         VStack{
             ZStack(alignment: .leading) {
@@ -20,8 +22,8 @@ struct PreviaReceitaView: View {
             ZStack {
                 Color(AssetColor.blue_100)
                 detalhesVStack
-            }
-//            .navigationTitle("Banana com Nescau")
+            }.ignoresSafeArea()
+            //            .navigationTitle("Banana com Nescau")
         }
     }
     
@@ -30,11 +32,13 @@ struct PreviaReceitaView: View {
             VStack(spacing: 24) {
                 imageFood
                 HStack(spacing: 0) {
-                    detailsCard
-                    detailsCard
+                    timeDetailsCard
+                    dificultDetailsCard
                 }
                 bar
                 recipeDescription
+                bar
+                ingredientsTitle
                 Spacer()
             }
             .padding(16)
@@ -54,12 +58,60 @@ struct PreviaReceitaView: View {
             .padding([.leading,.trailing], 8)
     }
     
-    private var detailsCard: some View {
-        RoundedRectangle(cornerRadius: 8)
-            .frame(width: 163, height: 140)
-            .padding([.leading,.trailing], 8)
-            .foregroundColor(Color(AssetColor.blue_details))
-            .shadow(color: .gray, radius: 3, y: 2)
+    private var timeDetailsCard: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 8)
+                .frame(width: 163, height: 140)
+                .padding([.leading,.trailing], 8)
+                .foregroundColor(Color(AssetColor.blue_details))
+                .shadow(color: .gray, radius: 3, y: 2)
+            
+            timeDetails
+            
+        }
+    }
+    
+    private var dificultDetailsCard: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 8)
+                .frame(width: 163, height: 140)
+                .padding([.leading,.trailing], 8)
+                .foregroundColor(Color(AssetColor.blue_details))
+                .shadow(color: .gray, radius: 3, y: 2)
+            
+            dificultDetails
+            
+        }
+    }
+    
+    private var dificultDetails: some View {
+        VStack{
+            Image("dificult")
+                .resizable()
+                .frame(width: 67,height: 51)
+                .foregroundColor(Color(red: 0.11, green: 0.29, blue: 0.40, opacity: 1.00))
+                .bold()
+            
+            Text("Dificuldade")
+            
+            Text("Médio")
+                .bold()
+        }
+    }
+    
+    private var timeDetails: some View {
+        VStack{
+            Image(systemName: "alarm")
+                .resizable()
+                .frame(width: 47,height: 52)
+                .foregroundColor(Color(red: 0.11, green: 0.29, blue: 0.40, opacity: 1.00))
+                .bold()
+            
+            Text("Duração")
+            
+            Text("10 minutos")
+                .bold()
+        }
     }
     
     private var bar: some View {
@@ -69,6 +121,30 @@ struct PreviaReceitaView: View {
     
     private var recipeDescription: some View {
         Text("Essa é uma receita de uma deliciosa banana com leite em pó, Nescau e farinha láctea. Um lanche prático, rápido e muito divertido de fazer!! ")
+    }
+    
+    private var ingredientsTitle: some View {
+        VStack(spacing: 8 ) {
+            Text("Ingredientes")
+                .font(.title2)
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding([.leading,.trailing], 8)
+            
+            ingredients
+        }
+    }
+    
+    private var ingredients: some View {
+        VStack(alignment: .leading) {
+            ForEach(ingredientsList, id: \.self) { ingredient in
+                Text(" • " + ingredient)
+                    .font(.system(size: 17))
+                    .frame(maxWidth: .infinity,
+                           alignment: .leading)
+                    .padding([.leading,.trailing], 8)
+            }
+        }
     }
 }
 
